42eb80495310ae488ef2772da33f5bac31f7b7aa
Manuel Messon-Roque <manuelmesson@gmail.com>
2022-08-23 21:45:41 -0700

    clean up

--- pages/index.js
+++ pages/index.js
@@ -7,23 +7,8 @@ export default function Home() {
         <title>Manuel Messon-Roque</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <b>Manuel Messon-Roque</b>
-        <nav>
-          <ul>
-            <li>Blog</li>
-            <li>Resume</li>
-            <li>Projects</li>
-            <li>About</li>
-          </ul>
-        </nav>
-      </header>
-      <main></main>
 
-      <footer>
-        <a href="https://www.linkedin.com/in/manuelmesson/">Linkedin</a><br />
-        <a href="https://github.com/manuelMesson">GitHub</a>
-      </footer>
+      <main><b>Hello!</b></main>
     </div>
   );
 }
